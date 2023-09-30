6258ac54fa8f33faf08f9adf7e0a4eafda1e42c3
Gerson77 <gerson.snts55@gmail.com>
2023-09-30 05:49:06 -0300

    Add function toastEventManager

--- client/src/lib/EventManager.js
+++ client/src/lib/EventManager.js
@@ -1,38 +1,39 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {
-      addtoast: [],
-    };
+    this.listeners = new Map();
   }
 
   // inserir listener no evento
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   //  emit o evento
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
   }
+
+  //  remove o evento
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners.get(event);
+
+    if (!listeners) {
+      return;
+    }
+
+    const filteredListeners = listeners.filter(
+      (listener) => listener !== listenerToRemove,
+    );
+
+    this.listeners.set(event, filteredListeners);
+  }
 }
-
-const toastEventManager = new EventManager();
-
-toastEventManager.on('addtoast', (payload) => {
-  console.log('addtoast listener1', payload);
-});
-toastEventManager.on('addtoast', (payload) => {
-  console.log('addtoast listener2', payload);
-});
-
-toastEventManager.emit('addtoast', { type: 'danger', text: 'Texto' });
-console.log(toastEventManager);
